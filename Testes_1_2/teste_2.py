342b430b0ddf206b13d718f7395923df9df0f368
Pedro Mendes <99758612+mendes658@users.noreply.github.com>
2022-06-01 21:11:48 -0300

    Atualizando bug camelot

--- Testes_1_2/teste_2.py
+++ Testes_1_2/teste_2.py
@@ -1,35 +1,36 @@
 import zipfile
 import os
-import camelot  # são necessárias as libraries camelot e pandas
+import camelot 
 import pandas as pd
 
 
 def progresso(atual, total):
     porcentagem = round(atual / (total / 100), 2)
-    return porcentagem
-
+    barrinha = '{:<50}'.format(int(porcentagem/2)*'❚')
+    barra_comp = f'[{barrinha}] {porcentagem}% | [{atual}/{total}]'
+    return barra_comp
 
 with zipfile.ZipFile('anexos.zip', 'r') as anexos_zip:
     anexo_1 = (anexos_zip.namelist())[0]
     anexos_zip.extract(anexo_1)
 
-# o camelot lê as páginas especificadas e exporta em csv
-# em seguida o csv vira um dataframe e é concatenado com os outros
-# o máximo de otimização que eu consegui nessa tarefa foi com um 'for' para ler uma pag de cada vez
-# lendo todos de uma vez, o uso de memória escalava exponencialmente
-# provavelmente há um modo mais eficiente
+"""* O camelot irá ler as páginas especificadas nas variáveis 'pag_inicial' e 'pag_final' e transformar as tabelas em .csv, após cada leitura elas serão concatenadas num dataframe pandas.
+* O máximo de otimização que eu consegui nessa parte foi com um 'for' para ler uma por uma, provavelmente há um modo mais eficiente.
+* Em seguida as rows das colunas 'OD' e 'AMB' são alteradas no dataframe. Entendi essa tarefa de duas formas, explicitadas no código abaixo↓
+"""
+
 master_df = pd.DataFrame()
 pag_inicial = 3
 pag_final = 200
 ler = (pag_final-pag_inicial)+1
 for n in range(ler):
     tabelas = camelot.read_pdf(f'{anexo_1}', process_background=True,
-                               pages=f'{n + pag_inicial}')
+                               pages=f'{n + pag_inicial}', line_scale = 35)
     tabelas.export(f'tabela.csv', f='csv')
     master_df = pd.concat([master_df, pd.read_csv(f'tabela-page-{n + pag_inicial}-table-1.csv')])
     os.remove(f'tabela-page-{n + pag_inicial}-table-1.csv')
-    print(f'Criando tabelas: {progresso(n, ler)}% concluído. {n}/{ler}')
-print(f'100% concluído. {ler}/{ler}')
+    print(f'Criando tabelas: {progresso(n, ler)}', end='\r') #no google colab o "\r" para escrever em uma linha só não funcionou
+print(f'Criando tabelas: {progresso(ler, ler)}')
 
 # cria um novo pandas dataframe, itera sobre as rows e altera de acordo
 master_df.to_csv('tabela_unica.csv', index=False)
